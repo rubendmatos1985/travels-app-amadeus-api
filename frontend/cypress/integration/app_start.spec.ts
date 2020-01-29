07319a27a8f755fc00bbe426bfc1e8954e9cf425
ruben1985 <rubendmatos1985@gmail.com>
2020-01-29 16:04:17 +0100

    prettier added

--- frontend/cypress/integration/app_start.spec.ts
+++ frontend/cypress/integration/app_start.spec.ts
@@ -1,11 +1,11 @@
 describe('app start', () => {
   it('home as starting point', () => {
-    cy.visit('localhost:8080')
-    cy.url().should('include', 'home')
-  })
+    cy.visit('localhost:8080');
+    cy.url().should('include', 'home');
+  });
 
   it('redirects to home if no registered endpoint is entered', () => {
-    cy.visit('localhost:8080/yukjhgfdfghjklkjhgf')
-    cy.url().should('include', 'home')
-  })
-})
+    cy.visit('localhost:8080/yukjhgfdfghjklkjhgf');
+    cy.url().should('include', 'home');
+  });
+});
--- frontend/cypress/integration/search_panel.spec.ts
+++ frontend/cypress/integration/search_panel.spec.ts
@@ -1,18 +1,41 @@
-const {
-  selectOptionFromAutocomplete
-} = require('../../../frontend/cypress/commons/select_option_from_autocomplete')
 describe('<SearchPanel/>', () => {
   beforeEach(() => {
-    cy.visit('localhost:8080')
-  })
+    cy.visit('localhost:8080');
+  });
 
   it(
     'Origin Input -> select option from autocomplete',
     selectOptionFromAutocomplete({ tag: '#orgin-input', option: 'Berlin' })
-  )
-  it('Destination Input -> select option from autocomplete', () => {})
+  );
+  it('Destination Input -> select option from autocomplete', () => {});
 
   it('Destination input-> do not have orgin input in suggestions list', () => {
-    const input = cy.get('#origin-input')
-  })
-})
+    const input = cy.get('#origin-input');
+  });
+});
+
+interface ISelectOptionFromAutocompleteArgs {
+  tag: string;
+  option: string;
+}
+
+const selectOptionFromAutocomplete = ({
+  tag,
+  option
+}: ISelectOptionFromAutocompleteArgs) => (): Cypress.Chainable<JQuery<HTMLElement>> =>
+  cy
+    .get(tag)
+    .type(option)
+    .then(
+      ($input: JQuery<HTMLInputElement>): Cypress.Chainable<JQuery<HTMLElement>> =>
+        cy
+          .get('.MuiAutocomplete-listbox')
+          .children()
+          .first()
+          .click({ force: true })
+          .then(($li: JQuery<HTMLLIElement>): void => {
+            const li: HTMLLIElement = $li.toArray()[0];
+            const input: HTMLInputElement = $input.toArray()[0];
+            expect(input.value).to.be.eq(li.innerText.split(' ').join(', '));
+          })
+    );
