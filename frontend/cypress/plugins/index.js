92f0464159461c2440bc0d0a5b27bed648664c5c
ruben1985 <rubendmatos1985@gmail.com>
2020-01-29 11:59:59 +0100

    unused imports

--- frontend/cypress/plugins/index.js
+++ frontend/cypress/plugins/index.js
@@ -1,6 +1,3 @@
-const fs = require('fs')
-const webpack = require('webpack')
-const path = require('path')
 const { typescriptCypressTranspiler } = require('typescript-cypress')
 const _module = {
   rules: [
